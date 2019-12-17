58bc8d4b1fc1c39119d4ac0dd7b368aec622f6f0
ndardagan <nadja.dardagan@gmail.com>
2019-12-17 14:59:19 +0100

    Active actions done

--- CharityProject/CharityData/Models/CharityAction.cs
+++ CharityProject/CharityData/Models/CharityAction.cs
@@ -10,16 +10,23 @@ namespace CharityData.Models
         public Guid Id { get; set; }
         [Required]
         [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters.")]
+        [Display(Name = "Action name")]
         public string name { get; set; }
         [Required]
         [StringLength(200, ErrorMessage = "Description cannot be longer than 200 characters.")]
+        [Display(Name = "Description")]
         public string description { get; set; }
         [Required]
+        [Display(Name = "Type of action")]
         public bool actionType { get; set; } // 0 - participation ; 1 - donation
         [Required]
         public Guid organizationId { get; set; }
         public DateTime creationDateTime { get; set; }
+
+        [Display(Name = "Start date")]
         public DateTime startDateTime { get; set; }
+
+        [Display(Name = "End date")]
         public DateTime endDateTime { get; set; }
     }
 }
